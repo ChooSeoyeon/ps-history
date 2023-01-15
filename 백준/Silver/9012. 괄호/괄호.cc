e6781537c682c32d4ee950c820f3310e845b604d
Choo <83302344+ChooSeoyeon@users.noreply.github.com>
2023-01-15 20:08:47 +0900

    Update 괄호.cc

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,7 +1,24 @@
 #include<iostream>
 #include <string>
 #include <stack>
+#include <algorithm>
+#include<vector>
+
 using namespace std;
+typedef long long ll;
+/*
+[문제 설명]
+- 괄호 문자열 : 두 개의 괄호 기호만으로 구성된 문자열
+- 올바른 괄호 문자열(VPS) : 괄호의 모양이 바르게 구성된 문자열
+- VPS 속의 VPS, VPS끼리 접합 가능
+- 주어진 괄호 문자열이 vps면 YES, 아니면 NO 출력
+[문제 접근]
+- 스택 이용
+- 1. 여는 괄호 나오면 push
+- 2. 닫는 괄호 나오면 스택이 비어있으면 push, 비어있지 않다면 pop해서 현재 닫는괄호와 짝지어주기
+- 괄호문자열의 모든 문자를 대상으로 앞에서부터 순서대로 검사해 1이나 2 적용
+- 모든 검사가 끝났을 때 스택이 비어있으면 모두가 짝을 찾아간 것이므로 YES, 비어있지 않다면 NO
+*/
 
 int main()
 {
@@ -23,7 +40,7 @@ int main()
 				}
 				s.pop();
 			}
-			if(ch=='(') s.push(ch);
+			if (ch == '(') s.push(ch);
 		}
 
 		if (s.empty()) cout << "YES\n";

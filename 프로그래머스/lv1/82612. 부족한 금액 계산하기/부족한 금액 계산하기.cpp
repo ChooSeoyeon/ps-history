0053b7696f7ff4dd32addee35e938c12f251701a
Choo <choo000407@naver.com>
2023-04-14 21:36:03 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 using namespace std;
 
 /*
@@ -8,22 +7,12 @@ using namespace std;
 - count번 탔을 때 현재 갖고 있는 금액에서 얼마가 모자라는지 return
 - 금액 안모자라면 0 return
 [문제 접근]
-- 
+- 반복문 혹은 가우스 공식 사용
+- 조건연산자 사용 -> (조건식) ? 표현식1 : 표현식2;
 */
 
 long long solution(int price, int money, int count)
 {
-    long long answer = -1;
-    
-    long long sum = 0; // sum이 int면 price, count가 최대치일 때 음수 나옴 -> 1000000000-(-774309592)=-1774309592
-    // sum이 long이어야 제대로 계산됨 -> 1000000000-7815625000=6815625000
-    for(int i=1; i<=count; i++) {
-        sum += price*i;
-        // cout << price*i << endl;
-    }
-    cout << money << "-" << sum << "=" << (money - sum) * (-1);
-    if(money - sum < 0) answer = (money - sum) * (-1);
-    else answer = 0;
-
-    return answer;
+    long long sum=1LL*price*(count+1)*count/2;
+    return (money-sum<0)?(money-sum)*(-1):0;
 }

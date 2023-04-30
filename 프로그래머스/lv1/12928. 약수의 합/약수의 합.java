e93b26f47b9ee7ba1b47f887210c3eb2fc730c3a
Choo <choo000407@naver.com>
2023-05-01 00:12:59 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -3,12 +3,11 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1; i<=(int)Math.sqrt(n); i++) {
+        for(int i=1; i<=n/2; i++) { // 2의 짝꿍까지만 더해주면 됨. 그 다음은 1의 짝꿍인 n임.
             if(n%i==0) {
                 answer+=i;
-                if(n/i!=i) answer+=(n/i);
             }
         }
-        return answer;
+        return answer+n;
     }
 }

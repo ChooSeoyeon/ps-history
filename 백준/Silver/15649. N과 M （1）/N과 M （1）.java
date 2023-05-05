344b146960f6aa0a3d4daf99da44f39dcfda9b1c
Choo <choo000407@naver.com>
2023-05-06 04:13:10 +0900

    [Silver III] Title: N과 M (1), Time: 2568 ms, Memory: 63744 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+/*
+[설명]
+- 조합 (순서 달라도 하나로 봄) nCr
+[접근]
+- 백트래킹
+*/
+public class Main {
+    public static int n, r;
+    public static int[] arr = new int[9];
+    public static boolean[] visited = new boolean[9]; // false로 초기화
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        r = sc.nextInt();
+        backtracking(0);
+    }
+
+    public static void backtracking(int x) {
+        if(x==r) { // [종료 조건] r개 다 뽑았다면 출력하고 return
+            for(int i=0; i<r; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=1; i<=n; i++) { // [반복문] n개의 수 (1부터 N까지 중)
+            if(visited[i]) continue; // [제한조건] 이미 사용한 수는 사용 X
+            visited[i] = true; // [상태 변화] 사용한 수는 표시
+            arr[x] = i; // [현재 단계에서 해야 하는 작업] 정답에 집어 넣기
+            backtracking(x+1); // [다음 단계] 한 개의 수를 더 고름
+            visited[i] = false; // [원상 복구] 사용했다고 표시한 거 복구
+        }
+    }
+}

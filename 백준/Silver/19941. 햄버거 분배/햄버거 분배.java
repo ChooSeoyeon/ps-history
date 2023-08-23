33d85680ee97ab809d9f016f99c16ba65cd36524
Choo <choo000407@naver.com>
2023-08-24 00:58:29 +0900

    [Silver III] Title: 햄버거 분배, Time: 244 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.*;
+
+/*
+[설명]
+- 사람(P)과 햄버거(H)가 랜덤 순서로 일렬로 놓여 있음
+- 사람은 거리가 K 이하인 햄버거 먹을 수 있음
+- 식탁의 길이(N), 햄버거를 선택할 수 있는 거리(K), 사람과 햄버거 위치(HHPHPPHHPPHPPPHPHPHP) 주어졌을 때,
+- 햄버거 먹을 수 있는 사람의 최대 수 구하기
+[접근] 그리디
+- 각자의 자리에서 최대한 멀리있는 햄버거를 먹게 해주자.
+- 순서는 앞에서부터 가보자.
+- 먹을 수 있는 햄버거가 있으면, 햄버거를 N으로 바꿔주고 count를 1 올리자.
+*/
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.nextLine();
+        char[] ph = sc.next().toCharArray();
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (ph[i] == 'P' && canEat(i, k, ph)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static boolean canEat(int i, int k, char[] ph) {
+
+        for (int j = i - k; j <= i + k; j++) {
+            if (j == i) continue;
+            if (j >= 0 && j < ph.length) {
+                if (ph[j] == 'H') {
+                    ph[j] = 'N';
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

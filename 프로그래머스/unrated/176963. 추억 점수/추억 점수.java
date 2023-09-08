39217afc31e822215bf38a663570b5e96c9e4156
Choo <choo000407@naver.com>
2023-09-08 16:58:46 +0900

    [unrated] Title: 추억 점수, Time: 0.05 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -8,35 +8,25 @@
 - photo 순서대로 추억 점수 배열에 담아 return
 [접근] HashMap, HashSet 사용
 - name(key)과 yearing(value)을 HashMap에 저장
-- name을 HashSet에 저장
-- HashSet을 HashSet에 저장
 - name과 photo 비교해서 겹치는 원소 구하기. 
-- 겹치는 원소로 HashMap에서 value값 찾기
-- value값들 합해서 result에 저장.
+- 겹치는 원소로 HashMap에서 yearing 값 찾기
+- yearing 합해서 result에 저장.
 */
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
+        int[] answer = new int[photo.length];      
+        HashMap<String, Integer> map = new HashMap<>();
         
-        HashMap<String, Integer> yearningMap = new HashMap<>();
-        HashSet<String> nameSet = new HashSet<>();
-        
-        for(int i=0; i<name.length; i++) {
-            yearningMap.put(name[i], Integer.valueOf(yearning[i]));
-            nameSet.add(name[i]);
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], Integer.valueOf(yearning[i]));
         }
         
-        for(int i=0; i<photo.length; i++) {
-            HashSet<String> photoSet = new HashSet<>();
+        for (int i = 0; i < photo.length; i++) {
             int sum = 0;
-            for(String personName : photo[i]) {
-                photoSet.add(personName);
-            }
-            photoSet.retainAll(nameSet);
-            for(String key: photoSet) {
-                sum += (int)yearningMap.get(key);
+            for (int j = 0; j < photo[i].length; j++) {
+                sum += map.getOrDefault(photo[i][j], 0);
             }
             answer[i] = sum;
         }

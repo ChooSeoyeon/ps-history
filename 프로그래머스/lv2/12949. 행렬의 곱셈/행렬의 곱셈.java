5a916d7adb5ab774787db20bbb39ace724a30415
Choo <choo000407@naver.com>
2023-09-13 14:36:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.37 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,6 +4,10 @@
 - 곱할 수 있는 배열만 주어짐
 - arr1, arr2[2[-10..20]..100][2..100]
 [접근]
+- arr1의 행(가로)과 arr2의 열(세로)을 곱함
+- answer의 행 개수는 arr1의 행 개수(arr1.length)와 같고, -> i
+- answer의 열 개수는 arr2의 열 개수(arr2[0].length)와 같음 -> j
+- 각 행과 열을 곱할 땐[i][j] arr1의 열 개수(== arr2의 행 개수)만큼 반복하며 각 원소를 곱해주면 됨 -> k
 */
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
@@ -12,7 +16,7 @@ class Solution {
         for(int i=0; i<arr1.length; i++) {
             for(int j=0; j<arr2[0].length; j++) {
                 int sum = 0;
-                for(int k=0; k<arr1[0].length; k++) {
+                for(int k=0; k<arr2.length; k++) {
                     sum += arr1[i][k] * arr2[k][j];
                 }
                 answer[i][j] = sum;

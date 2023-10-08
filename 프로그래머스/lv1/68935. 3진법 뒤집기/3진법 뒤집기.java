618abe16d22931cdddc187770c386f33d88bc9f3
Choo <choo000407@naver.com>
2023-10-09 00:26:13 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,3 +1,11 @@
+/*
+[설명]
+- 10진법 숫자를 3진법으로 변환 후 뒤집고, 다시 10진법으로 변환해 반환
+[접근]
+- 10진법 -> 3진법 : String str = Integer.toString(25, 3);
+- 3진법 -> 10진법 : int num = Integer.parseInt("10001", 3);
+*/
+
 import java.util.*;
 
 class Solution {

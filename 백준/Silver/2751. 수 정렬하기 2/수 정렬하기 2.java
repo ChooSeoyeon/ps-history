dc03a76531eab09bf50417da3a524a5306dbad50
Dora Choo <choo000407@naver.com>
2024-01-21 23:27:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1040 ms, Memory: 109872 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 /*
 [설명]
@@ -13,24 +12,60 @@ import java.util.Arrays;
 	- 두 정렬된 배열을 하나의 정렬된 배열로 합치기
 [메모]
 [오답]
+- sout 썼다가 시간 초과 남. 개행 문자 따로 빼도 시간 초과 남
+- bufferWriter 써도 시간 초과 남
+- 배열을 로컬 변수로 각각 가지고 있다가 인자로 넘겨주는 방식에서, static 변수로 갖고 있게 바꿨더니 시간 초과 해결됨
 */
 public class Main {
-	private static void solution(int n, int[] numbers) {
-		Arrays.sort(numbers);
+	private static int[] numbers = new int[1_000_001];
+	private static int[] temp = new int[1_000_001];
+
+	private static void solution(int n) {
+		mergeSort(0, n);
 	}
-    
+
+	private static void mergeSort(int start, int end) {
+		if (end - start == 1) {
+			return;
+		}
+		int mid = (start + end) / 2;
+		mergeSort(start, mid);
+		mergeSort(mid, end);
+		merge(start, end);
+	}
+
+	private static void merge(int start, int end) {
+		int mid = (start + end) / 2;
+		int left = start;
+		int right = mid;
+		for (int i = start; i < end; i++) {
+			if (left == mid) {
+				temp[i] = numbers[right++];
+			} else if (right == end) {
+				temp[i] = numbers[left++];
+			} else if (numbers[left] <= numbers[right]) { // 합병 정렬의 stable sort 성질 만족시키기 위해서 크기 같을 땐 앞 쪽에 들어가게 해줌
+				temp[i] = numbers[left++];
+			} else {
+				temp[i] = numbers[right++];
+			}
+		}
+		for (int i = start; i < end; i++) {
+			numbers[i] = temp[i];
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int n = Integer.parseInt(br.readLine());
-		int[] numbers = new int[n];
 		for (int i = 0; i < n; i++) {
 			numbers[i] = Integer.parseInt(br.readLine());
 		}
-		solution(n, numbers);
+		solution(n);
 
-		for (int number : numbers) {
-			bw.write(number + "\n");
+		for (int i = 0; i < n; i++) {
+			bw.write(numbers[i] + "\n");
 		}
 
 		bw.flush();

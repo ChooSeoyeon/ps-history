cbf0a46dbc8491e863342dc527a96adffd6a43f2
Dora Choo <choo000407@naver.com>
2024-04-12 01:30:06 +0900

    [Silver II] Title: 키트 분배하기, Time: 400 ms, Memory: 37076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24938. 키트 분배하기/키트 분배하기.java
+++ 백준/Silver/24938. 키트 분배하기/키트 분배하기.java
@@ -8,12 +8,17 @@ import java.io.InputStreamReader;
 - 키트는 바로 옆방에만 넘겨줄 수 있고, 바로 옆방으로 키트 한 개를 건네줄 때 혼잡도가 1 증가함
 - 최대한 가까운 방끼리 키트 주고받을 때, 혼잡도가 최소임. 혼잡도의 최솟값 구하기
 [접근]
-- 앞에서부터 순서대로 보기
-    - 예시: 2 6 3 2 5 4 6 -> 기준값: 28/7 = 4
-    - [0] 은 [1] 로부터 2개 받는다 -> 4 4 3 2 5 4 6 -> 2
-    - [2] 은 [4] 로부터 1개 받는다 -> 4 4 4 2 4 4 6 -> 2
-    - [3] 은 [6] 로부터 2개 받는다 -> 4 4 4 4 4 4 4 -> 6
-[메모]
+- 앞에서부터 순서대로 보기. 무조건 다음 친구한테 떠넘겨서 본인 값을 기준값과 동일하게 맞추기
+- 예시: 2 6 3 2 5 4 6 -> 기준값: 28/7 = 4
+    - [0] 은 [1] 로부터 2개 받는다 -> 4 4 3 2 5 4 6
+    - [2] 은 [3] 로부터 1개 받는다 -> 4 4 4 1 5 4 6
+    - [3] 은 [4] 로부터 3개 받는다 -> 4 4 4 4 2 4 6
+    - [4] 은 [5] 로부터 2개 받는다 -> 4 4 4 4 4 2 6
+    - [5] 은 [6] 로부터 2개 받는다 -> 4 4 4 4 4 4 4
+- 예시: 6 2 2 2 -> 기준값: 12/4 = 3
+    - [0] 은 [1] 에게 3개 준다 -> 3 5 2 2
+    - [1] 은 [2] 에게 2개 준다 -> 3 3 4 2
+    - [2] 은 [1] 에게 1개 준다 -> 3 3 3 3
 [오답]
 - 처음에 사용했던 방식
     - 넘치는 애들과 부족한 애들을 각자의 인덱스로 넘치거나 부족한 값만큼 반복해 더하기
@@ -23,28 +28,25 @@ import java.io.InputStreamReader;
         - 계산: (1-0)+(1-0)+(4-2)+(6-3)+(6-3)= 18 - 8 = 10
     - 각각 빼고 더했을 땐 시간복잡도 초과 떠서, 한 번에 더하고 한 번에 뺌
     - (2 2 6 2) 은 4가 나와야 하는데 2가 나오는 반례를 찾음
-    - 해당 방식으론 불가한 거 깨달아서 그리디로 방향 바꿈
-- int 써서 틀렸어서 long 씀
+    - 해당 방식으론 시간 안에 불가한 거 깨달아서 그리디로 방향 바꿈
 */
 public class Main {
-    private static long solution(int n, int[] numbers) {
+    private static long solution(int n, long[] numbers) {
         long sum = 0;
-        for (int number : numbers) {
+        for (long number : numbers) {
             sum += number;
         }
         long target = sum / n;
 
         long answer = 0;
-        for (int i = 0; i < n; i++) {
-            int j = 0;
-            while (numbers[i] < target) {
-                if (numbers[j] > target) {
-                    numbers[j]--;
-                    numbers[i]++;
-                    answer += Math.abs(i - j);
-                } else {
-                    j++;
-                }
+        for (int i = 0; i < n - 1; i++) {
+            long gap = target - numbers[i];
+            numbers[i + 1] -= gap;
+            if (gap > 0) {
+                answer += gap;
+            }
+            if (gap < 0) {
+                answer -= gap;
             }
         }
 
@@ -56,7 +58,7 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] numbers = new int[n];
+        long[] numbers = new long[n];
         String[] input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             numbers[i] = Integer.parseInt(input[i]);
